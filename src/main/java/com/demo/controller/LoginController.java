a059bc39f1f8420e1721a42790779937fc6f01bd
K-dhanush2468 <kd9344551324@gmail.com>
2025-07-14 10:51:41 +0530

    commit message

---
+++ src/main/java/com/demo/controller/LoginController.java
@@ -0,0 +1,46 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	@GetMapping(value = "/login")
+	public String getLogin(@RequestParam String a, @RequestParam String b) {
+
+		if (a.equals("dhanush") && b.equals("123")) {
+			return "Login accept";
+		} else {
+			return "Login not accept";
+		}
+
+	}
+	
+	@GetMapping(value = "/max/{a}")
+	public int getPalidrom(@PathVariable int a, @RequestParam int b) {
+
+		if (a > b) {
+			return a;
+		} else {
+			
+			return b;
+		}
+
+	}
+	
+	@GetMapping(value = "/max1/{a}/{b}")
+	public int getMax(@PathVariable int a, @PathVariable int b) {
+
+		if (a > b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+	
+
+}
+
